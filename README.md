c14235bb637efb6642b62cce25ffd0baa7476be3
Hatsune Miku <l2a1knla@gmail.com>
2024-06-17 02:54:47 +0800

    doc: Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ## KProfiler
+
+为 html_pc 项目提供性能监控服务，支持以下功能：
+
+- CPU 占用率采集
+- GPU 占用率采集
+- RAM 数值采集
+- 写入到 csv
+- 可视化
+
+---
+
+以同样基于 electron 的微信小程序 PC 版为例：
+
+![Demo 1](doc/demo1.jpg)
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 #!python3
 # encoding:utf-8
 
-# 1，后台运行，获取kook进程的cpu OK，内存 OK，GPU OK 占用率或者数值？
-# 2，由于kook有很多进程，希望能做到 分进程统计 OK 并且 计算总值（只能获取总值也行，但是感觉做不到）
-# 3，1S获取一次 OK
-# 4，写入本地文件，结束后针对数据总值 绘制曲线图（能做成实时绘制的那就更好啦）
-
 from core.kprofiler import KProfiler
 from core.history import History
 from server.dash_server import DashServer
