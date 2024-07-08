e27fd86c749c4feadca5ef5dd02489547b68ba09
Hatsune Miku <l2a1knla@gmail.com>
2024-07-08 20:00:47 +0800

    fix: try fix cpu thread error

--- helpers/performance_counter_win.py
+++ helpers/performance_counter_win.py
@@ -219,7 +219,7 @@ class PerformanceCounter:
             if pid not in self.cpu_process_recent_history:
                 self.cpu_process_recent_history[pid] = []
             self.cpu_process_recent_history[pid].append(percent_value)
-            percent_value = sum(self._get_recent_n_records(pid, 5))
+            percent_value = sum(self._get_recent_n_records(pid, 6))
             pid_to_cpu_percent_map[pid] = percent_value
 
         return pid_to_cpu_percent_map
